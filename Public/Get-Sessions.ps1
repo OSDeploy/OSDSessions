2eea2b3eae6b165117b53d19a7f4406a44b6e57a
David Segura <david@segura.org>
2019-03-27 12:02:46 -0500

    19.3.27.0

--- Public/Get-Sessions.ps1
+++ Public/Get-Sessions.ps1
@@ -1,51 +1,24 @@
 ﻿function Get-Sessions {
     [CmdletBinding()]
-    PARAM ([ValidateSet('CumulativeUpdate','Update')]
-        [string]$Type,
+    PARAM (
         [switch]$GridView
     )
+	[xml]$XmlDocument = Get-Content -Path "$env:SystemRoot\Servicing\Sessions\Sessions.xml"
 
-    BEGIN {
-        #Write-Host '========================================================================================' -ForegroundColor DarkGray
-        #Write-Host "$($MyInvocation.MyCommand.Name) BEGIN" -ForegroundColor Green
-
-        #===================================================================================================
-        Write-Verbose '19.1.1 Initialize OSBuilder'
-        #===================================================================================================
-        $Sessions = @()
-        [xml]$XmlDocument = Get-Content -Path 'C:\Windows\Servicing\Sessions\Sessions.xml'
+    $Sessions = $XmlDocument.SelectNodes('Sessions/Session') | ForEach-Object {
+        New-Object -Type PSObject -Property @{
+            Id = $_.Tasks.Phase.package.id
+            KBNumber = $_.Tasks.Phase.package.name
+            TargetState = $_.Tasks.Phase.package.targetState
+            Client = $_.Client
+            Complete = $_.Complete
+            Status = $_.Status
+        }
     }
 
-    PROCESS {
-        #Write-Host '========================================================================================' -ForegroundColor DarkGray
-        #Write-Host "$($MyInvocation.MyCommand.Name) PROCESS" -ForegroundColor Green
+    $Sessions = $Sessions | Where-Object {$_.Id -like "Package*"}
+    $Sessions | Select-Object -Property Id, KBNumber, TargetState, Client, Status, Complete | Sort-Object Complete -Descending
 
-		[xml]$XmlDocument = Get-Content -Path 'C:\Windows\servicing\Sessions\Sessions.xml'
-
-$Sessions = $XmlDocument.SelectNodes('Sessions/Session') | ForEach-Object {
-    New-Object -Type PSObject -Property @{
-        Id = $_.Tasks.Phase.package.id
-        KBNumber = $_.Tasks.Phase.package.name
-        TargetState = $_.Tasks.Phase.package.targetState
-        Client = $_.Client
-        Complete = $_.Complete
-        Status = $_.Status
-    }
-}
-
-$Sessions = $Sessions | Where-Object {$_.Id -like "Package*"}
-
-$Sessions | Select -Property Id, KBNumber, TargetState, Client, Status, Complete | Sort-Object Complete -Descending
-		
-        $Sessions = $XmlDocument.Sessions.Session.Tasks.Phase.package
-        $Sessions = $Sessions | Select -Property Id, Name, TargetState, Options
-        $Sessions = $Sessions | Where-Object {$_.id -like "Package*"}
-    }
-
-    END {
-        #Write-Host '========================================================================================' -ForegroundColor DarkGray
-        #Write-Host "$($MyInvocation.MyCommand.Name) END" -ForegroundColor Green
-        if ($GridView.IsPresent) {Return $Sessions | Select -Property * | Out-GridView}
-        else {Return $Sessions}
-    }
+    if ($GridView.IsPresent) {Return $Sessions | Select-Object -Property * | Out-GridView}
+    else {Return $Sessions}
 }
